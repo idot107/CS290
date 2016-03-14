63a645dc8cf8b1454c8c6b6353de81024a2280cb
Kevin <demingk@oregonstate.edu>
2016-03-13 23:24:34 -0700

    Update SQL.js

--- dbint/SQL.js
+++ dbint/SQL.js
@@ -10,18 +10,21 @@ app.set('port',3000);
 
 app.get('/reset-table',function(req,res,next){
   var context = {};
-  mysql.pool.query("DROP TABLE IF EXISTS todo", function(err){
-    var createString = "CREATE TABLE todo(" +
-    "id INT PRIMARY KEY AUTO_INCREMENT," +
-    "name VARCHAR(255) NOT NULL," +
-    "done BOOLEAN," +
-    "due DATE)";
-    mysql.pool.query(createString, function(err){
+  [your connection pool].query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
+    var createString = "CREATE TABLE workouts("+
+    "id INT PRIMARY KEY AUTO_INCREMENT,"+
+    "name VARCHAR(255) NOT NULL,"+
+    "reps INT,"+
+    "weight INT,"+
+    "date DATE,"+
+    "lbs BOOLEAN)";
+    [your connection pool].query(createString, function(err){
       context.results = "Table reset";
       res.render('home',context);
     })
   });
 });
+
 
 app.get('/',function(req,res,next){
 var context = {};
